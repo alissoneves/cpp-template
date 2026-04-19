af2995eeb2da6e42cc12e849365ad8869212d76b
alissoneves2 <alidoidosw@gmail.com>
2026-04-19 19:04:35 -0300

    Update main cpp to check the pipeline with webhook working

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Let's get it started! :)\n";
     std::cout << "Let's get it started 2! :)\n";
     std::cout << "Let's get it started 3! :)\n";
+    std::cout << "Let's get it started 4! :)\n";
     return 0;
 }
